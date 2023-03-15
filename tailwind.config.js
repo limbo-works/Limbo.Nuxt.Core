75d9f596105f194d4adeb1836a89fdc86cded6b1
Simon Erslev Milfred <smi@limbo.works>
2023-03-15 13:24:24 +0100

    feat: reduced the tailwind configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -497,7 +497,7 @@ module.exports = {
 			max: 'max-content',
 			fit: 'fit-content',
 		}),
-		maxWidth: ({ theme, breakpoints }) => ({
+		maxWidth: () => ({
 			none: 'none',
 			0: '0px',
 			full: '100%',
@@ -505,7 +505,6 @@ module.exports = {
 			min: 'min-content',
 			max: 'max-content',
 			fit: 'fit-content',
-			...breakpoints(theme('screens')),
 			// Removed some values
 		}),
 		minHeight: {
@@ -665,25 +664,7 @@ module.exports = {
 			...theme('spacing'),
 		}),
 		spacing: {
-			0: '0px',
-			1: '1px',
-			2: '2px',
-			3: '3px',
-			4: '4px',
-			5: '5px',
-			6: '6px',
-			7: '7px',
-			8: '8px',
-			9: '9px',
-			10: '10px',
-			11: '11px',
-			12: '12px',
-			14: '14px',
-			15: '15px',
-			16: '16px',
-			20: '20px',
-			24: '24px',
-			// Removed and changed some values
+			// Removed values
 		},
 		stroke: ({ theme }) => ({
 			none: 'none',
