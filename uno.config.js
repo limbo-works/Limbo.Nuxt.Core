e9fcc5e0b664a9b49bd127ee1992dd193691448b
Simon Erslev Milfred <smi@limbo.works>
2023-06-07 14:56:07 +0200

    fix: wrong uno path

--- uno.config.js
+++ uno.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'unocss';
-import presetCore from '../assets/js/unocss/preset-core.js';
-import presetNoDefaultRem from '../assets/js/unocss/preset-no-default-rem.js';
+import presetCore from './assets/js/unocss/preset-core.js';
+import presetNoDefaultRem from './assets/js/unocss/preset-no-default-rem.js';
 
 export default defineConfig({
 	presets: [
