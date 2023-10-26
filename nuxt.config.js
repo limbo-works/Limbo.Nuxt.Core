c2fdff921df44908ed39cd9cf6cb84664aed07e3
Simon Erslev Milfred <smi@limbo.works>
2023-10-26 09:05:06 +0200

    fix: should remove dimensions on svgs

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,7 +34,7 @@ export default defineNuxtConfig({
 						},
 						{
 							name: 'removeDimensions',
-							active: false,
+							active: true,
 						},
 						{
 							name: 'addAttributesToSVGElement',
