d52335cc2e9cae69382db1a7f1fc62af3b77dc45
Simon Milfred <smi@limbo.works>
2024-05-29 09:19:18 +0200

    fix: improved app history composable slightly

--- composables/useAppHistory.js
+++ composables/useAppHistory.js
@@ -26,23 +26,19 @@ export const useAppHistory = () => {
 		},
 	});
 
-	onMounted(() => {
+	onBeforeMount(() => {
 		dataObject.ready = true;
 
+		update();
+	});
+	watch(() => route.fullPath, update);
+
+	function update() {
 		dataObject.backUrl = router.options.history.state.back;
 		dataObject.currentUrl = router.options.history.state.current;
 		dataObject.forwardUrl = router.options.history.state.forward;
 		dataObject.position = router.options.history.state.position;
-	});
-	watch(
-		() => route.fullPath,
-		() => {
-			dataObject.backUrl = router.options.history.state.back;
-			dataObject.currentUrl = router.options.history.state.current;
-			dataObject.forwardUrl = router.options.history.state.forward;
-			dataObject.position = router.options.history.state.position;
-		}
-	);
+	}
 
 	return dataObject;
 };
