1fcdfd20673ec36e2138ed329751fccccdc87f04
Simon Milfred <smi@limbo.works>
2026-01-16 09:11:42 +0100

    fix: added some off-hooking for useEventBus

--- composables/useEventBus.js
+++ composables/useEventBus.js
@@ -1,15 +1,32 @@
 export function useEventBus() {
 	const app = useNuxtApp();
+	const registeredHooks = new Set();
+
+	// Cleanup hooks when component is unmounted
+	onBeforeUnmount(() => {
+		registeredHooks.forEach((hookName) => {
+			app.hooks.removeHook(hookName);
+		});
+		registeredHooks.clear();
+	});
 
 	return {
 		on(event, callback) {
 			if (typeof callback === 'function') {
-				app.hook(`limbo:${event}`, callback);
+				const hookName = `limbo:${event}`;
+				app.hook(hookName, callback);
+				registeredHooks.add(hookName);
 			}
 		},
 
 		emit(event, data) {
 			app.hooks.callHook(`limbo:${event}`, data);
+		},
+
+		off(event, callback) {
+			const hookName = `limbo:${event}`;
+			app.hooks.removeHook(hookName, callback);
+			registeredHooks.delete(hookName);
 		},
 	};
 }
